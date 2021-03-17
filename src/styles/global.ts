12bec61b019324a0ee473dbe9c9f5c0cda1eb8ba
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-03-17 12:41:25 -0300

    ✨ Add: dashboard & summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,7 @@ export const GlobalStyle = createGlobalStyle`${css`
     --background: #f0f2f5;
     --red: #e52e4d;
     --blue: #5429cc;
+    --green: #33cc95;
     --blue-light: #6933ff;
     --text-title: #363f5f;
     --text-body: #969cb3;
