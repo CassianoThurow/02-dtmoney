538e903108bd33611bb3d3d09c6f0ae89794aea9
Cassiano Thurow <cassiano.thurow@gmail.com>
2021-03-17 08:43:09 -0300

    🎨 Clear boilerplate & add global style

---
+++ src/styles/global.ts
@@ -0,0 +1,60 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`${css`
+  :root {
+    --background: #f0f2f5;
+    --red: #e52e4d;
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --background: #fbf2f5;
+    --shape: #ffffff;
+  }
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body,
+  input,
+  textarea,
+  button {
+    font-family: 'poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  strong {
+    font-weight: 600;
+  }
+
+  body {
+    background: var(--background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`}`;
